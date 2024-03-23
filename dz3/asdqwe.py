4e4bb7417b5fc8974db25b2a43d10534860a5601
maksimbusko <kallutoname@gmail.com>
2024-03-23 13:04:58 +0200

    finished

--- dz3/asdqwe.py
+++ dz3/asdqwe.py
@@ -8,15 +8,16 @@ print(sys.argv)
 path = Path(sys.argv[1])
 
 def print_directory_contents(path, indent=''):
-    if path.is_dir():
-        for item in path.iterdir():
-            if item.is_dir():
-                print(f"{indent}{Fore.BLUE}📂 {item.name}/")
-                print_directory_contents(item, indent + '   ')
-            else:
-                print(f"{indent}{Fore.GREEN}📜 {item.name}")
-    else:
-        print(f"{Fore.RED}Шлях '{path}' не є директорією.")
+    if path.exists():
+        if path.is_dir():
+            for item in path.iterdir():
+                if item.is_dir():
+                    print(f"{indent}{Fore.BLUE}📂 {item.name}/")
+                    print_directory_contents(item, indent + '   ')
+                else:
+                    print(f"{indent}{Fore.GREEN}📜 {item.name}")
+        else:
+            print(f"{Fore.RED}Шлях '{path}' не є директорією.")
 
 directory_path = path
 print_directory_contents(directory_path)
